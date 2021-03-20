15ef3167e988a56964467d314e762cfa84c84c65
j-james <jj@j-james.me>
2021-03-19 19:57:15 -0700

    Initial commit

---
+++ src/main/java/com/spartronics4915/frc/subsystems/ExampleSubsystem.java
@@ -0,0 +1,27 @@
+package com.spartronics4915.frc.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Detailed description of ExampleSubsystem.
+ */
+public class ExampleSubsystem extends SubsystemBase
+{
+    // The subsystem's hardware is defined here...
+
+    /** Creates a new ExampleSubsystem. */
+    public ExampleSubsystem()
+    {
+        // ...and constructed here.
+    }
+
+    // Subsystem methods - actions the robot can take - should be placed here.
+
+    /** This method will be called once per scheduler run. */
+    @Override
+    public void periodic() {}
+
+    /** This method will be called once per scheduler run during simulation. */
+    @Override
+    public void simulationPeriodic() {}
+}
